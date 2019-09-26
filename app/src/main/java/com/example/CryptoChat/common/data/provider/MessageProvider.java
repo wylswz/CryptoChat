cebb634159661faae5569432e47f47f0d937759e
yunlu <wylswz@icloud.com>
2019-09-26 11:38:02 +1000

    Greendao setup

--- app/src/main/java/com/example/CryptoChat/common/data/provider/MessageProvider.java
+++ app/src/main/java/com/example/CryptoChat/common/data/provider/MessageProvider.java
@@ -1,6 +1,20 @@
 package com.example.CryptoChat.common.data.provider;
 
-public class MessageProvider {
+import com.example.CryptoChat.common.data.models.Message;
+
+import java.util.Date;
+import java.util.List;
+
+public abstract class MessageProvider {
+    abstract public Message getMessageById(String Id);
+    abstract public Message getMessageByPk(Long pk);
+    abstract public List<Message> getMessages(Date from, Date to, Long userDbId);
+    abstract public List<Message> getMessages(Long userDbId);
+
+    abstract public void InsertMessage(Message m);
+    abstract public void DropMessageById(String Id);
+    abstract public void DropMessageByPk(Long pk);
+
 
 
 }
