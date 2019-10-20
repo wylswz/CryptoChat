54d37b922d5297c97e17fe360820d66870875cfa
kelly330 <55571438+kelly330@users.noreply.github.com>
2019-10-20 12:00:37 +1100

    Update SendMsgDetail.java

--- app/src/main/java/com/example/CryptoChat/services/SendMsgDetail.java
+++ app/src/main/java/com/example/CryptoChat/services/SendMsgDetail.java
@@ -19,7 +19,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.R;
+import com.example.CryptoChat.R;
 import com.example.CryptoChat.services.SendMsg;
 import com.example.CryptoChat.services.User;
 
