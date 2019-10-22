1715ce7bda6b6b6b5fe74598b983de2fea964bf1
kelly330 <55571438+kelly330@users.noreply.github.com>
2019-10-22 19:41:26 +1100

    Add files via upload

---
+++ app/src/main/java/com/example/CryptoChat/services/FriendChatService.java
@@ -0,0 +1,190 @@
+package com.example.CryptoChat.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Base64;
+import android.util.Log;
+//import android.support.v7.app.NotificationCompa;
+import android.app.Notification.Builder;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.example.CryptoChat.controllers.MainActivity;
+import com.example.CryptoChat.R;
+import com.example.CryptoChat.common.data.models.Friend;
+import com.example.CryptoChat.DB.StaticConfig;
+import com.example.CryptoChat.common.data.models.ListFriend;
+import com.example.CryptoChat.DB.FriendDB;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class FriendChatService extends Service {
+    private static String TAG = "FriendChatService";
+    // Binder given to clients
+    public final IBinder mBinder = new LocalBinder();
+    public Map<String, Boolean> mapMark;
+    public Map<String, Query> mapQuery;
+    public Map<String, ChildEventListener> mapChildEventListenerMap;
+    public Map<String, Bitmap> mapBitmap;
+    public ArrayList<String> listKey;
+    public ListFriend listFriend;
+    public CountDownTimer updateOnline;
+
+    public FriendChatService() {
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mapMark = new HashMap<>();
+        mapQuery = new HashMap<>();
+        mapChildEventListenerMap = new HashMap<>();
+        listFriend = FriendDB.getInstance(this).getListFriend();
+        listKey = new ArrayList<>();
+        mapBitmap = new HashMap<>();
+        updateOnline = new CountDownTimer(System.currentTimeMillis(), StaticConfig.TIME_TO_REFRESH) {
+            @Override
+            public void onTick(long l) {
+                ServiceUtils.updateUserStatus(getApplicationContext());
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        updateOnline.start();
+
+        if (listFriend.getListFriend().size() > 0) {
+            //Dang ky lang nghe cac room tai day
+            for (final Friend friend : listFriend.getListFriend()) {
+                if (!listKey.contains(friend.idRoom)) {
+                    mapQuery.put(friend.idRoom, FirebaseDatabase.getInstance().getReference().child("message/" + friend.idRoom).limitToLast(1));
+                    mapChildEventListenerMap.put(friend.idRoom, new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                            if (mapMark.get(friend.idRoom) != null && mapMark.get(friend.idRoom)) {
+//                                Toast.makeText(FriendChatService.this, friend.name + ": " + ((HashMap)dataSnapshot.getValue()).get("text"), Toast.LENGTH_SHORT).show();
+                                if (mapBitmap.get(friend.idRoom) == null) {
+                                    if (!friend.avata.equals(StaticConfig.STR_DEFAULT_BASE64)) {
+                                        byte[] decodedString = Base64.decode(friend.avata, Base64.DEFAULT);
+                                        mapBitmap.put(friend.idRoom, BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length));
+                                    } else {
+                                        mapBitmap.put(friend.idRoom, BitmapFactory.decodeResource(getResources(), R.drawable.default_avatar));
+                                    }
+                                }
+                               // createNotify(friend.name, (String) ((HashMap) dataSnapshot.getValue()).get("text"), friend.idRoom.hashCode(), mapBitmap.get(friend.idRoom), false);
+
+                            } else {
+                                mapMark.put(friend.idRoom, true);
+                            }
+                        }
+
+                        @Override
+                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                    listKey.add(friend.idRoom);
+                }
+                mapQuery.get(friend.idRoom).addChildEventListener(mapChildEventListenerMap.get(friend.idRoom));
+            }
+
+
+        } else {
+            stopSelf();
+        }
+    }
+
+    public void stopNotify(String id) {
+        mapMark.put(id, false);
+    }
+/***
+    public void createNotify(String name, String content, int id, Bitmap icon, boolean isGroup) {
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, activityIntent, PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notificationBuilder = new
+                NotificationCompat.Builder(this)
+                .setLargeIcon(icon)
+                .setContentTitle(name)
+                .setContentText(content)
+                .setContentIntent(pendingIntent)
+                .setVibrate(new long[] { 1000, 1000})
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setSmallIcon(R.drawable.ic_tab_person)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(
+                        Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(id);
+        //notificationManager.notify(id,
+         //       notificationBuilder.build());
+    }
+ */
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "OnStartService");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "OnBindService");
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        for (String id : listKey) {
+            mapQuery.get(id).removeEventListener(mapChildEventListenerMap.get(id));
+        }
+        mapQuery.clear();
+        mapChildEventListenerMap.clear();
+        mapBitmap.clear();
+        updateOnline.cancel();
+        Log.d(TAG, "OnDestroyService");
+    }
+
+    public class LocalBinder extends Binder {
+        public FriendChatService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return FriendChatService.this;
+        }
+    }
+}
