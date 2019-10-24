575feea4b896bd25e4e6cdafdeca90dd263cffbe
kelly330 <55571438+kelly330@users.noreply.github.com>
2019-10-24 19:35:47 +1100

    Add files via upload

---
+++ app/src/main/java/com/example/CryptoChat/controllers/LightSensorActivity.java
@@ -0,0 +1,55 @@
+package com.example.CryptoChat.controllers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import com.example.CryptoChat.R;
+
+public class LightSensorActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor light;
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        float light_now = event.values[0];
+
+        if (light_now < 100){
+            // TODO: pop out a reccomentdation to change a background or etc.
+        }else if(light_now > 50000){
+            // TODO: pop out a reccomentdation to change a background or etc.
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
---
+++ app/src/main/java/com/example/CryptoChat/controllers/SpeedSensorActivity.java
@@ -0,0 +1,67 @@
+package com.example.CryptoChat.controllers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.CryptoChat.R;
+
+import static java.lang.Math.sqrt;
+
+public class SpeedSensorActivity extends Activity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private TextView mTxtValue;
+    private Sensor mSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // sensor manager
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        // TYPE_ACCELEROMETER
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // TYPE_LIGHT
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // add listener for mSensor
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    // call when the value of sensor changed
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] values = event.values;
+        int x = (int) values[0];
+        int y = (int) values[1];
+        int z = (int) values[2];
+        double accelerometer = sqrt(x^2+y^2+z^2);
+        double speed = 3 * accelerometer;
+
+        if (speed > 3){
+            // TODO: add reccomendation on Screen(walk too fast, plz change to voice talk or sth)
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+}
